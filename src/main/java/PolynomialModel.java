083c1d960580dd95f79640a78dd42dbf6632578f
Robert B <rorakex@icloud.com>
2022-02-21 13:50:16 +0200

    adunare2

--- src/main/java/PolynomialModel.java
+++ src/main/java/PolynomialModel.java
@@ -10,22 +10,27 @@ public class PolynomialModel {
             lowerGrade = toAdd;
         }
         int prevDeg= biggerGrade.getDeg();
+        boolean added;
         for (Monomial y : biggerGrade.getComponents()) {
+            added=false;
             for (Monomial z : lowerGrade.getComponents()) {
                 if (z.getDeg() == y.getDeg()) {
-                    tempResult.getComponents().add(new Monomial(z.getDeg(), z.getCoeff() + y.getCoeff()));
+                    tempResult.getComponents().add(new Monomial(z.getDeg(), z.getCoeff() + y.getCoeff())); added=true;
                     break;
                 } else if (z.getDeg() < y.getDeg()) {
-                    tempResult.getComponents().add(new Monomial(y.getDeg(), y.getCoeff()));
+                    tempResult.getComponents().add(new Monomial(y.getDeg(), y.getCoeff()));added=true;
                 }
                 else
                     if(z.getDeg()<prevDeg)
-                        tempResult.getComponents().add(new Monomial(z.getDeg(), z.getCoeff()));
+                        tempResult.getComponents().add(new Monomial(z.getDeg(), z.getCoeff()));added=true;
                 }
-
+            if(added==false)
+                tempResult.getComponents().add(y);
             }
+        currentResult=tempResult;
         }
     }
+
 
 
 
