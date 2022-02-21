3def179b1756468962c890cfbd41a0cf29bef80a
Robert B <rorakex@icloud.com>
2022-02-21 12:33:49 +0200

    initial commit

---
+++ src/main/java/PolynomialMVC.java
@@ -0,0 +1,7 @@
+public class PolynomialMVC {
+    public static void main(String[] args) {
+        PolynomialModel pCalcModel = new PolynomialModel();
+        PolynomialController pCalcController = new PolynomialController();
+
+    }
+}
